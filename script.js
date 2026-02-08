284533040c605f6f2e51282780de4fe0f9dabd4d
BatKaGrip14 <tchate377@gmail.com>
2026-02-08 18:48:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const screens = document.querySelectorAll(".screen");
+
 const clickSound = document.getElementById("clickSound");
 const yesSound = document.getElementById("yesSound");
 const noSound = document.getElementById("noSound");
 
 function showScreen(id) {
-  screens.forEach(s => s.classList.remove("active"));
+  screens.forEach(screen => screen.classList.remove("active"));
   document.getElementById(id).classList.add("active");
 }
 
-document.getElementById("toScreen2").onclick = () => {
+// Screen navigation
+document.getElementById("toScreen2").addEventListener("click", () => {
   clickSound.play();
   showScreen("screen2");
-};
+});
 
-document.getElementById("toScreen3").onclick = () => {
+document.getElementById("toScreen3").addEventListener("click", () => {
   clickSound.play();
   showScreen("screen3");
-};
+});
 
-document.getElementById("toScreen4").onclick = () => {
+document.getElementById("toScreen4").addEventListener("click", () => {
   clickSound.play();
   showScreen("screen4");
-};
+});
 
-document.getElementById("noBtn").onclick = () => {
+// No button
+document.getElementById("noBtn").addEventListener("click", () => {
   noSound.play();
-};
+});
 
-document.getElementById("yesBtn").onclick = () => {
+// Yes button
+document.getElementById("yesBtn").addEventListener("click", () => {
   yesSound.play();
 
   const avatar = document.getElementById("avatar");
@@ -36,4 +40,4 @@ document.getElementById("yesBtn").onclick = () => {
   setTimeout(() => {
     showScreen("screen5");
   }, 2500);
-};
+});
